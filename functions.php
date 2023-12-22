4d4cb5372cddafca1abb6020c5fd7aa4a4407f35
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2023-12-22 11:33:15 +0530

    Add Style and Scripts
    Add stylesheet and scripts using wp_register_script and
    wp_enqueue_script and same for stylesheet.

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@
 
 
 // Includes
-include(get_theme_file_path('/includes/front/enqueue.php')); 
+include(get_theme_file_path('/includes/front/enqueue.php'));
 
 // Hooks
-add_action('wp_enqueue_scripts', 'ju_enqueue');
+add_action('wp_enqueue_scripts', 'su_enqueue');
 
 // Shortcodes
