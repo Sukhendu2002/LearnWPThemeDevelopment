b0ec97bf8844609873078019c4bff9c85539bce1
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2023-12-22 14:13:27 +0530

    Add a Nav Menu
    Register a Nav Menu using register_nav_menu() function
    Display the menu using the wp_nav_menu() function
    
    Ref:
    https://developer.wordpress.org/reference/functions/register_nav_menu/
    https://developer.wordpress.org/reference/functions/wp_nav_menu/

--- functions.php
+++ functions.php
@@ -5,8 +5,10 @@ define('SU_DEV_MODE', true);
 
 // Includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
+include(get_theme_file_path('/includes/setup.php'));
 
 // Hooks
 add_action('wp_enqueue_scripts', 'su_enqueue');
+add_action('after_setup_theme', 'su_setup');
 
 // Shortcodes
---
+++ includes/setup.php
@@ -0,0 +1,9 @@
+<?php
+
+function su_setup()
+{
+    register_nav_menu(
+        'primary',
+        __('Primary Menu', 'udemy')
+    );
+}
